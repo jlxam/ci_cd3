7ac71cdab3a6106db5802248e7a7b62819d44cdc
Antony <antony.jeyaraj@trenser.com>
2026-09-08 22:43:08 +0530

    update4

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -10,7 +10,20 @@ test.describe('Admin Dashboard', { tag: ['@smoke', '@admin'] }, () => {
   });
 
   test('Zdmin can update system settings',{ tag: '@admin' }, async ({ page }) => {
-    // This test also inherits @admin and @regression
+    // This test also inherits @admin and @regression 
+    await page.goto('https://demoqa.com/automation-practice-form')
+    await page.locator(`#firstName`).fill('Antony');
+    await page.getByPlaceholder('Last Name').fill('password123');
+    await page.getByPlaceholder('name@example.com').fill('Antony@gmail.com');
+    const element = page.locator('#gender-radio-2');
+    await element.click();
+    await page.getByPlaceholder("Mobile Number").fill('9588555811');
+    await page.getByPlaceholder('Last Name').fill('password123');
+    await page.locator('#subjectsInput').fill('Antony');
+    //const element1 = page.getByLabel('Sports')
+    //const element1 =page.locator('#hobbies-checkbox-2')
+    const element1 = page.locator('#hobbiesWrapper').getByLabel('Music')
+    await element1.click();
     console.log('Running system settings test.2..');
   });
 });
